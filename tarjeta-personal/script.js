cba154c41347b46547260b1c6c84387a096f2d32
Koretto777 <182255233+koretto777@users.noreply.github.com>
2025-07-01 10:57:12 -0600

    cambio dinamico de icon-theme y marca grafica

--- tarjeta-personal/script.js
+++ tarjeta-personal/script.js
@@ -2,10 +2,16 @@
 
 const toggleButton = document.getElementById("toggle-button");
 const body = document.body;
+const logo = document.getElementById("logo")
 
 /* función para ejecutar un código */
 
 toggleButton.addEventListener("click", () => {
-   body.classList.toggle("dark")
-})
+   body.classList.toggle("dark");
 
+   /* cambiamos el logo segun el tema */
+
+   const isDark = body.classList.contains("dark");
+
+   logo.src = isDark ? "./assets/mood=dark.svg" : "./assets/logo koretto.svg"
+})
